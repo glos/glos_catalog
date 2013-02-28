2d4c4e4c394c688f6dcb76db4046a587b7a214c1
Sean Cowan <scowan@asascience.com>
2013-02-28 12:39:22 -0500

    aoc iso collector

---
+++ pyiso/pyiso/tests/aoc/test_aoc.py
@@ -0,0 +1,10 @@
+import unittest
+from pyiso.aoc.aoc import AOCCollector
+
+class AOCTest(unittest.TestCase):
+
+	def test_aoc(self):
+		aoc = AOCCollector()
+		aoc.download_isos()
+
+		pass
--- pyiso/pyiso/tests/utils/test_utils.py
+++ pyiso/pyiso/tests/utils/test_utils.py
@@ -1,23 +1,23 @@
 import unittest
 from urllib import unquote
 
-from pyiso.utils.iso_download import ISODownload
+from pyiso.utils.iso_download import download_iso
 
 class UtilTest(unittest.TestCase):
 	def setUp(self):
-		self._isodl = ISODownload()
+		pass
 
 	def test_download(self):
 		"""
 			download from the lake michigan nowcast 3d 2006 best model
 		"""
 
-		print 'starting test_download'
+		pass
 
 		url = 'http://64.9.200.113:8080/thredds/iso/glos/glcfs/archive2006/michigan/ncfmrc-3d/Lake_Michigan_-_Nowcast_-_3D_-_2006_best.ncd'
 		catalog = unquote('http%3A%2F%2F64.9.200.113%3A8080%2Fthredds%2Fcatalog%2Fglos%2Fglcfs%2Farchive2006%2Fmichigan%2Fncfmrc-3d%2Fcatalog.html')
 		dataset = unquote('glos%2Fglcfs%2Farchive2006%2Fmichigan%2Fncfmrc-3d%2FLake_Michigan_-_Nowcast_-_3D_-_2006_best.ncd')
 
-		file_name = self._isodl.download_iso(url, catalog=catalog, dataset=dataset)
+		file_name = download_iso(url, catalog=catalog, dataset=dataset)
 
-		print file_name
+		assert file_name == 'Lake_Michigan_-_Nowcast_-_3D_-_2006_best.xml'
