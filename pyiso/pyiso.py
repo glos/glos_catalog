36e2c570fda7d47d59aef2205eec53c9e8f7e28e
Kyle Wilcox <kwilcox@asascience.com>
2013-05-02 15:09:23 -0400

    Implement an IsoDownloader class and callback functions

--- pyiso/pyiso.py
+++ pyiso/pyiso.py
@@ -1,9 +1,9 @@
 from pyiso.collectors.thredds import ThreddsCollector
 from pyiso.collectors.geo import GeoCollector
 from pyiso.collectors.insitu import InSituCollector
+from pyiso.downloader import IsoDownloader
 
 import requests
-from urlparse import urlsplit
 import os
 import codecs
 
@@ -18,68 +18,46 @@ def main():
     datasets = ["SST-Agg", "SST-FMRC"]
     # Don't process the "files/" lists
     skips = ["files/"]
-    aoc = ThreddsCollector("http://tds.glos.us:8080/thredds/aoc.html", datasets=datasets, skips=skips).run()
-    for d in aoc:
-        f = os.path.basename(urlsplit(d).path) + ".xml"
-        with open(os.path.join(base_download_path, "Satellite", f), "w") as handle:
-            handle.write(requests.get(d).text)
-    
-    
+    isos = ThreddsCollector("http://tds.glos.us:8080/thredds/aoc.html", datasets=datasets, skips=skips).run()
+    download_path = os.path.join(base_download_path, "Satellite")
+    IsoDownloader.run(isos, download_path)
+
     # We only want the all year best datasets
     datasets = ["2D_best", "3D_best", "2D_-_All_Years_best", "3D_-_All_Years_best"]
     # Don't process the "files/" lists
     skips = ["files/"]
-    aoc = ThreddsCollector("http://tds.glos.us:8080/thredds/glcfs/glcfs.html", datasets=datasets, skips=skips).run()
-    for d in aoc:
-        f = os.path.basename(urlsplit(d).path) + ".xml"
-        with open(os.path.join(base_download_path, "Models", "GLCFS", f), "w") as handle:
-            handle.write(requests.get(d).text)
+    isos = ThreddsCollector("http://tds.glos.us:8080/thredds/glcfs/glcfs.html", datasets=datasets, skips=skips).run()
+    download_path = os.path.join(base_download_path, "Models", "GLCFS")
+    IsoDownloader.run(isos, download_path)
 
+    # We only want the Agg and Latest
+    datasets = ["Nowcast-Agg", "Lastest-Forecast"]
+    # Don't process the "files/" lists
+    skips = ["Nowcast - Individual Files/", "Forecast - Individual Files/"]
+    isos = ThreddsCollector("http://tds.glos.us:8080/thredds/hecwfs/hecwfs.html", datasets=datasets, skips=skips).run()
+    download_path = os.path.join(base_download_path, "Models", "HECWFS")
+    IsoDownloader.run(isos, download_path)
     
     # We only want the Agg and Latest
     datasets = ["Nowcast-Agg", "Lastest-Forecast"]
     # Don't process the "files/" lists
     skips = ["Nowcast - Individual Files/", "Forecast - Individual Files/"]
-    aoc = ThreddsCollector("http://tds.glos.us:8080/thredds/hecwfs/hecwfs.html", datasets=datasets, skips=skips).run()
-    for d in aoc:
-        f = os.path.basename(urlsplit(d).path) + ".xml"
-        with open(os.path.join(base_download_path, "Models", "HECWFS", f), "w") as handle:
-            handle.write(requests.get(d).text)
-    
-    
-    # We only want the Agg and Latest
-    datasets = ["Nowcast-Agg", "Lastest-Forecast"]
-    # Don't process the "files/" lists
-    skips = ["Nowcast - Individual Files/", "Forecast - Individual Files/"]
-    aoc = ThreddsCollector("http://tds.glos.us:8080/thredds/slrfvm/slrfvm.html", datasets=datasets, skips=skips).run()
-    for d in aoc:
-        f = os.path.basename(urlsplit(d).path) + ".xml"
-        with open(os.path.join(base_download_path, "Models", "SLRFVM", f), "w") as handle:
-            handle.write(requests.get(d).text)
-    
-    
+    isos = ThreddsCollector("http://tds.glos.us:8080/thredds/slrfvm/slrfvm.html", datasets=datasets, skips=skips).run()
+    download_path = os.path.join(base_download_path, "Models", "SLRFVM")
+    IsoDownloader.run(isos, download_path)
+
     isos = GeoCollector("http://data.glos.us/metadata").run()
-    for d in isos:
-        # Get the ID
-        uid = urlsplit(d).query
-        uid = uid[uid.index("=")+1:]
-        f = "GeoNetwork-" + uid + ".xml"
-        new_file = os.path.join(base_download_path, "GeoNetwork", f)
-        with codecs.open(new_file, "w", "utf-8") as handle:
-            handle.write(requests.get(d).text)
+    download_path = os.path.join(base_download_path, "GeoNetwork")
+    IsoDownloader.run(isos, download_path, namer=GeoCollector.namer, modifier=GeoCollector.modifier)
     
     isos = InSituCollector("http://64.9.200.113:8080/isos", "ndbc").run()
-    for d in isos:
-        f = os.path.basename(urlsplit(d).path)
-        with open(os.path.join(base_download_path, "In-situ", "ndbc", f), "w") as handle:
-            handle.write(requests.get(d).text)
+    download_path = os.path.join(base_download_path, "In-situ", "ndbc")
+    IsoDownloader.run(isos, download_path)
     
-
     isos = InSituCollector("http://64.9.200.113:8080/isos", "storet").run()
-    for d in isos:
-        f = os.path.basename(urlsplit(d).path)
-        with open(os.path.join(base_download_path, "In-situ", "storet", f), "w") as handle:
-            handle.write(requests.get(d).text)
+    download_path = os.path.join(base_download_path, "In-situ", "storet")
+    IsoDownloader.run(isos, download_path)
+    
 
 main()
 
--- pyiso/pyiso/collectors/geo.py
+++ pyiso/pyiso/collectors/geo.py
@@ -5,6 +5,7 @@ import os
 import csv
 import tempfile
 import codecs
+from urlparse import urlsplit
 
 class GeoCollector(object):
     def __init__(self, base_url):
@@ -35,3 +36,26 @@ class GeoCollector(object):
         os.unlink(f.name)
 
         return isos
+
+    @staticmethod
+    def namer(url, **kwargs):
+        uid = urlsplit(url).query
+        uid = uid[uid.index("=")+1:]
+        return "GeoNetwork-" + uid + ".xml"
+
+    @staticmethod
+    def modifier(url, **kwargs):
+        # translate ISO19139 to ISO19115
+        gmi_ns = "http://www.isotc211.org/2005/gmi"
+        etree.register_namespace("gmi",gmi_ns)
+
+        new_root = etree.Element("{%s}MI_Metadata" % gmi_ns)
+        old_root = etree.parse(url).getroot()
+
+        # carry over an attributes we need
+        [new_root.set(k,v) for k,v in old_root.attrib.items()]
+        # carry over children
+        [new_root.append(e) for e in old_root]
+
+        return etree.tostring(new_root, encoding="UTF-8", pretty_print=True, xml_declaration=True)
+                
