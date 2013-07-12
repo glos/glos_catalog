c57b0cd0c2d8f31feaf36208cc775544c825cad1
Kyle Wilcox <kwilcox@asascience.com>
2013-07-12 16:23:25 -0400

    Remove the script to convert geonetwork files (its a callback in pyiso now)

--- convert_geonetwork.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for f in `find ISOs/GeoNetwork -name metadata.xml`; do
-    sed -i -e 's/gmd:MD_Metadata/gmi:MI_Metadata/g' -e 's/gmi:MI_Metadata\ /gmi:MI_Metadata\ xmlns:gmi="http:\/\/www.isotc211.org\/2005\/gmi"\ /g' $f
-done
