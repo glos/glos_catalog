4ece6e8903860cba4f67340b87c1af3b65b847f4
Kyle Wilcox <kwilcox@asascience.com>
2013-07-29 13:21:45 -0400

    Finish renaming the GeoNetworkCollector

--- metadown/download.py
+++ metadown/download.py
@@ -60,7 +60,7 @@ def main(base_download_path):
 
     isos = GeoNetworkCollector("http://data.glos.us/metadata").run()
     download_path = os.path.join(base_download_path, "GeoNetwork")
-    XmlDownloader.run(isos, download_path, namer=GeoCollector.namer, modifier=GeoCollector.modifier)   
+    XmlDownloader.run(isos, download_path, namer=GeoNetworkCollector.namer, modifier=GeoNetworkCollector.modifier)   
 
 main(sys.argv[1])
 
