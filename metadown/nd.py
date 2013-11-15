6fe34bb4ec34d202cf69384e8b646ec7acffc339
GLOS Harvester <maracoos-glos@asa.flowdock.com>
2013-11-15 21:47:07 +0000

    GLERL metadown manual

--- metadown/nd.py
+++ metadown/nd.py
@@ -16,6 +16,28 @@ def main(base_download_path):
     download_path = os.path.join(base_download_path, "RANGER3")
     XmlDownloader.run(isos, download_path)
 
+    # GLERL
+    selects = [".*"]
+    isos = ThreddsCollector("http://tds.glos.us/thredds/glerl/NBS.html", selects=selects).run()
+    download_path = os.path.join(base_download_path, "GLERL", "NBS")
+    XmlDownloader.run(isos, download_path)
+
+    selects = [".*"]
+    isos = ThreddsCollector("http://tds.glos.us/thredds/glerl/ATMO.html", selects=selects).run()
+    download_path = os.path.join(base_download_path, "GLERL", "ATMO")
+    XmlDownloader.run(isos, download_path)
+
+    selects = [".*"]
+    isos = ThreddsCollector("http://tds.glos.us/thredds/glerl/PRECIP.html", selects=selects).run()
+    download_path = os.path.join(base_download_path, "GLERL", "PRECIP")
+    XmlDownloader.run(isos, download_path)
+
+    selects = [".*"]
+    isos = ThreddsCollector("http://tds.glos.us/thredds/glerl/AIRTEMPS.html", selects=selects).run()
+    download_path = os.path.join(base_download_path, "GLERL", "AIRTEMPS")
+    XmlDownloader.run(isos, download_path)
+
+
 main(sys.argv[1])
 
 exit(0)
